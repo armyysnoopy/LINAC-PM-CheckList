f6877650ab7b631fef1df8663e1f29fad3ded155
armyysnoopy <54132341+armyysnoopy@users.noreply.github.com>
2019-11-06 12:03:45 +0700

    Add files via upload

---
+++ site1/rvh_linac03.php
@@ -0,0 +1,317 @@
+<?php
+//ตั้งค่าการเชื่อมต่อฐานข้อมูล
+    $database_host             = 'localhost';
+    $database_username         = 'root';
+    $database_password         = 'pbiservice'; //set password at privileges on phpmyadmin
+    $database_name             = 'lampang_cancer_hospital';
+    $data ;
+    $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
+
+//กำหนด charset ให้เป็น utf8 เพื่อรองรับภาษาไทย
+    $mysqli->set_charset("utf8");
+ 
+//กรณีมี Error เกิดขึ้น
+    if ($mysqli->connect_error) {
+        die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+    }
+    
+//เรียกข้อมูลจาก ตาราง linac_versahd_154406 
+    $get_data = $mysqli->query("SELECT * FROM linac_versahd_154406 WHERE DatePM = '2019-08-18'");
+        while($data = $get_data->fetch_assoc()){
+            $result[] = $data;
+            printf ("%s %s \n", $data["Input_to_stabilizer_L1"], $data["Input_to_stabilizer_L2"]);
+        }
+          
+    //print json_encode($result[0]);    
+    //printf ("%s %s \n", $data["Input_to_stabilizer_L1"], $data["Input_to_stabilizer_L2"]);
+        
+/* close connection */
+    $mysqli->close();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Linac RVH | Service Engineer</title>
+        <link rel="stylesheet" href="_css/html5structure.css" type="text/css">
+        <meta charset="utf-8">
+        <style>
+            table, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+            table tr:nth-child(even) {
+                background-color: #eee;
+            }
+            table tr:nth-child(odd) {
+                background-color: #fff;
+            }
+            table th {
+                background-color: darkblue;
+                color: white;
+            }
+          </style>
+    </head>
+    <body>
+    <table style="width:100%" align="center">
+        <caption><h3>PM LINAC CHECKLIST</h3></caption>
+    <!-- Title -->  
+        <tr align="center">
+            <th>Catalogue</th>
+            <th>Description</th>
+            <th>Specification</th>
+            <th>Measured</th>
+            <th>Investigate/Adjustment</th>
+            <th>Result</th>
+        </tr>         
+    <!-- Gantry -->  
+        <tr>
+        <td rowspan="2" style="text-align:center">Gantry</td>
+            <td>Gantry angle zero</td>
+            <td>น้อยกว่าเท่ากับ 1.0 องศา</td>
+            <?php
+                    foreach($result as $result_tb){
+                            echo "<td>".$result_tb['Input_to_stabilizer_L2']."</td>";
+                            echo "<td>".$result_tb['Input_to_stabilizer_L3']."</td>";                           
+                    }
+                ?>
+            
+            <td>Result</td> 
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td>Gantry angle at 0, 90, 180, 270</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Collimator head -->    
+        <tr>
+            <td rowspan="6" style="text-align:center">Collimator head</td>
+            <td>Collimator angle</td>
+            <td>1.0 องศา</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Crosswire screen</td>
+            <td>1.0 องศา(SSD 100), 2mm(on floor)</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Distance meter</td>
+            <td>1mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Laser ที่ Isocenter</td>
+            <td>1mm (Optional check with ball bearing)</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Field size</td>
+            <td>2mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>The Camera Cond</td>
+            <td>Not for Agility Head</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Precise Table -->
+        <tr>
+            <td rowspan="5" style="text-align:center">Precise Table</td>
+            <td>Table height readout error</td>
+            <td>1 mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Table lateral readout error</td>
+            <td>1 mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Table long readout error</td>
+            <td>1 mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Check Table Isocentric</td>
+            <td>1 mm</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Motor Brush</td>
+            <td>4 mm (Recommend to replace)</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Dielectric Gas System -->
+        <tr>
+            <td style="text-align:center">Dielectric Gas System</td>
+            <td>Pressure system SF6 check</td>
+            <td>Baseline</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Main Supply -->
+        <tr>
+            <td rowspan="6" style="text-align:center">Main Supply</td>
+            <td rowspan="3"><a href="db_lampang03.php" target="_blank">Input to Stabilizer</a></td>
+            <td rowspan="3">Functional</td>
+            <td>L1 = <?php
+                    foreach($result as $result_tb){
+                            echo $result_tb['Input_to_stabilizer_L1'];
+                        }
+                    ?> 
+                Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>L2 = ... Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>L3 = ... Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td rowspan="3">Output to Stabilizer</td>
+            <td rowspan="3">Functional</td>
+            <td>L1 = ... Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>L2 = ... Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>L3 = ... Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Thyratron Check -->
+        <tr>
+            <td rowspan="5" style="text-align:center">Thyratron Check</td>
+            <td>Replenisher</td>
+            <td>Hydrogen Heater voltage (5.2 to 5.8 Vac)</td>
+            <td>...Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Heater</td>
+            <td>Cathode heater voltage (6.0 to 6.6 Vac)</td>
+            <td>...Vac</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Grid1</td>
+            <td>Grid1 voltage (16.5 to 19.5 Vdc)</td>
+            <td>...Vdc</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td rowspan="2">Grid2</td>
+            <td rowspan="2">Grid2 bias voltage (-120 to -135 Vdc)(-400 Hz)</td>
+            <td>...Vdc</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>...Hz</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Gun I Mon 6MV-->
+        <tr>
+            <td style="text-align:center">Gun I Mon 6MV</td>
+            <td>(i217, p4)</td>
+            <td>Baseline</td>
+            <td>last value</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Chiller -->
+        <tr>
+            <td rowspan="2" style="text-align:center">Chiller</td>
+            <td rowspan="2">Pressure water</td>
+            <td rowspan="2">Functional</td>
+            <td>Input ... Bar</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Output ... Bar</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- iView -->
+        <tr>
+            <td rowspan="2" style="text-align:center">iView</td>
+            <td>Imaging and Treatment coordinate coincidence</td>
+            <td>1mm (KV Flex map)</td>
+            <td>mm</td>
+            <td>ex. Run ball bearing</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Image Scalling (HorizMMPixel)</td>
+            <td>1mm</td>
+            <td>mm</td>
+            <td>ex. Recalibrate image scalling</td>
+            <td></td>
+        </tr>
+    <!-- XVI -->
+        <tr>
+            <td style="text-align:center">XVI</td>
+            <td>Imaging and Treatment coordinate isocenter</td>
+            <td>1mm (with image registration)</td>
+            <td>null</td>
+            <td></td>
+            <td></td>
+        </tr>
+    <!-- Linac Hour -->
+        <tr>
+            <td rowspan="2" style="text-align:center">Linac Hour</td>
+            <td rowspan="2">(LT i275, p4)(HT i276, p4)</td>
+            <td rowspan="2">Baseline</td>
+            <td>LT ...</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>HT ...</td>
+            <td></td>
+            <td></td>
+        </tr>
+        </table>
+    </body>
+</html>
